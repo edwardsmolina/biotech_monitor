5ae2fe51a64b35f850f6e627420f1e97e08d1f2c
Juan Pablo Edwards Molina <juanpablo.edwardsmolina@bayer.com>
2024-01-03 14:19:55 -0800

    added shp file
    Pushed from Domino: https://domino.science-at-scale.io/workspace/goxww/biotech_monitor_app?executionId=6595c2242becb2033eb4d898

--- app.R
+++ app.R
@@ -37,8 +37,8 @@ server <- function(input, output, session) {
       fitBounds(~min(long), ~min(lat), ~max(long), ~max(lat))
   })
   
-  # Incremental changes to the map (in this case, replacing the circles when a new color is chosen) should be performed in
-  # an observer. Each independent set of things that can change
+  # Incremental changes to the map (in this case, replacing the circles when a new color is chosen) 
+  # should be performed in an observer. Each independent set of things that can change
   # should be managed in its own observer.
   observe({
     pal <- colorpal()
